ceabfb9173276b13391951214d2aa3022452f0c2
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-01-03 19:58:34 +0100

    feat(exercise20.7): finish 'Constraining type helpers to non-empty arrays'

--- src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
+++ src/03.5-type-helpers-pattern/20.7-non-empty-array.problem.ts
@@ -1,4 +1,9 @@
-type NonEmptyArray = unknown;
+// Here we do something very interesting. We define a tuple in a type
+// called `NonEmptyArray` saying that it can be an array of any type T
+// but it cannot be empty. The array length must be > 1
+// What it says here is: `T` means it should at least have 1 argument
+// while `...Array<T>` says it might also have more args
+type NonEmptyArray<T> = [T, ...Array<T>];
 
 export const makeEnum = (values: NonEmptyArray<string>) => {};
 
