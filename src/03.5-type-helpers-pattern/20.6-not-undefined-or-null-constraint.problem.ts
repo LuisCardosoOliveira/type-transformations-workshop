3739c761e151d1f8141e3cdf6156c2bbcaefea73
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-01-03 19:51:39 +0100

    feat(exercise20.6): finish 'Constraining types for anything but null or undefined'

--- src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
+++ src/03.5-type-helpers-pattern/20.6-not-undefined-or-null-constraint.problem.ts
@@ -1,4 +1,7 @@
-export type Maybe<T> = T | null | undefined;
+// In this solution, we says that T extends an empty {}
+// because in TS, an `{}` means anything that is not `null`
+// neither `undefined`
+export type Maybe<T extends {}> = T | null | undefined;
 
 type tests = [
   // @ts-expect-error
@@ -9,5 +12,5 @@ type tests = [
   Maybe<string>,
   Maybe<false>,
   Maybe<0>,
-  Maybe<"">,
+  Maybe<"">
 ];
