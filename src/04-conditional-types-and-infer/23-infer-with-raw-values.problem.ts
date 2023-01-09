9534fc2831c88a0b508490219422d796373e1fd1
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-01-09 19:55:50 +0100

    feat(condtional-types): did exercise 23

--- src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
+++ src/04-conditional-types-and-infer/23-infer-with-raw-values.problem.ts
@@ -1,14 +1,17 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
-type GetDataValue<T> = unknown;
+type GetDataValue<T> = T extends { data: infer TInferredData } ? TInferredData : never;
+
+// This solution does not uses `infer`
+// type GetDataValue<T> = T extends { data: any } ? T["data"] : never;
 
 type tests = [
-  Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
-  Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
-  Expect<
-    Equal<
-      GetDataValue<{ data: { name: "hello"; age: 20 } }>,
-      { name: "hello"; age: 20 }
-    >
-  >,
+    Expect<Equal<GetDataValue<{ data: "hello" }>, "hello">>,
+    Expect<Equal<GetDataValue<{ data: { name: "hello" } }>, { name: "hello" }>>,
+    Expect<
+        Equal<
+            GetDataValue<{ data: { name: "hello"; age: 20 } }>,
+            { name: "hello"; age: 20 }
+        >
+    >,
 ];
