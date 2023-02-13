a1f99f12d4435d392b755eea583ca53e378e778b
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-02-13 19:45:31 +0100

    feat(mapped-types): did exercise 31

--- src/05-key-remapping/31-k-in-keyof-as.problem.ts
+++ src/05-key-remapping/31-k-in-keyof-as.problem.ts
@@ -6,8 +6,13 @@ interface Attributes {
   age: number;
 }
 
+
+// https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+// This is pretty awesome!
+// Here, we are using as as a key mapper. This gives us access to the original key from Attributes, while also allowing us to use it in a template literal.
+// Here we use a template literal to add the get prefix and the Capitalize string utility type to remap the key:
 type AttributeGetters = {
-  [K in keyof Attributes]: () => Attributes[K];
+  [K in keyof Attributes as `get${Capitalize<K>}`]: () => Attributes[K];
 };
 
 type tests = [
