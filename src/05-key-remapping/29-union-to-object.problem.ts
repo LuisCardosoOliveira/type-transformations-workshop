27679033dce4872f1b7acc4d61348678def289d6
Luis Cardoso <61982523+LuisCardosoOliveira@users.noreply.github.com>
2023-02-13 19:19:08 +0100

    feat(mapped-types): did exercise 29

--- src/05-key-remapping/29-union-to-object.problem.ts
+++ src/05-key-remapping/29-union-to-object.problem.ts
@@ -2,7 +2,12 @@ import { Equal, Expect } from "../helpers/type-utils";
 
 type Route = "/" | "/about" | "/admin" | "/admin/users";
 
-type RoutesObject = unknown;
+// The [Property in Route] part looks similar to an index, but it isn't.
+// tnstead, it's saying to map over the Route union extract each member into a
+// private variable called Property and make it the property's value.
+type RoutesObject = {
+    [Property in Route]: Property;
+};
 
 type tests = [
   Expect<
